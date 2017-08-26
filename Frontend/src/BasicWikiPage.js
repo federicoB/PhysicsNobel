d233f48ca97d3c612e654fdcdd23699aef2e1bac
federicoB <federicobertani96@gmail.com>
2017-08-26 11:19:55 +0200

    Implemented generic Wiki page.

--- Frontend/src/BasicWikiPage.js
+++ Frontend/src/BasicWikiPage.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import wiki from 'wikijs'
+import {Loader} from 'semantic-ui-react'
 
 export default class BasicWikiPage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            content : null
+        };
+        this.fetchContent(props.name)
+    }
+
+    fetchContent(pageName) {
+        wiki().page(pageName).then(page => page.content())
+            .then(content => this.setState({
+            content: content
+        }));
+    }
+
     render() {
-        return null;
+        const {content} = this.state;
+        return(<div>
+            {content!==null?content:<Loader active={true}/>}
+        </div>)
     }
 }

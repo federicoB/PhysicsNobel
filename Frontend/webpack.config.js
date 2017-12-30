04a39b81d04375c27b64421e0e79a1a6a406c870
federicoB <federicobertani96@gmail.com>
2017-12-30 16:56:37 +0100

    Improved webpack configuration with production / development differentiation
    In development limited transpiling to only es2016 and es2017 feature
    for speed up build.
    
    In production added minifing plugin and reduced bundle to size to about
    a third.

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const merge = require("webpack-merge")
 const BundleTracker = require('webpack-bundle-tracker');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const UglifyWebpackPlugin = require("uglifyjs-webpack-plugin");
 
-module.exports = {
+const commonConfig = {
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -11,20 +13,6 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
-                exclude: path.resolve(__dirname, 'node_modules'),
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ['react'],
-                            ['es2015', {modules: false}]
-                        ]
-                    }
-                }]
-            },
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
@@ -47,5 +35,68 @@ module.exports = {
     externals: { //required by webpack-bundle-tracker
         "isomorphic-fetch": "fetch"
     },
+};
+
+const productionConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, 'src'),
+                    //including also node_modules to be sure there will be no incompatibility with pre-es2015
+                    path.resolve(__dirname, 'node_modules')
+                ],
+                exclude: [
+                    //excluding wikijs because it was giving error with babel
+                    path.resolve(__dirname, 'node_modules/wikijs')
+                ],
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['react'],
+                            ['es2015', {modules: false}],
+                            ['es2016'],
+                            ['es2017']
+                        ]
+                    }
+                }]
+            },
+        ]
+    },
+    plugins: [
+        //javascript minifier
+        new UglifyWebpackPlugin()
+    ],
+};
+
+const developmentConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: path.resolve(__dirname, 'node_modules'),
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['react'],
+                            ['es2016'],
+                            ['es2017']
+                        ]
+                    }
+                }]
+            },
+        ]
+    },
     devtool: 'source-map'
+};
+
+module.exports = env => {
+    if (env === "production") {
+        return merge(commonConfig, productionConfig);
+    } else
+        return merge(commonConfig, developmentConfig);
 };

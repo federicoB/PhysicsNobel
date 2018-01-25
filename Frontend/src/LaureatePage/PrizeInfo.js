95f0d48f08c679665c091c1c72643a65fafc9610
federicoB <federicobertani96@gmail.com>
2018-01-25 12:42:56 +0100

    Improved prizeInfo component.
    Loader during request.
    'With' wasn't appearing when on two laureate prizes.

--- Frontend/src/LaureatePage/PrizeInfo.js
+++ Frontend/src/LaureatePage/PrizeInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Header,Container, Segment} from 'semantic-ui-react';
+import {Header, Container, Segment, Loader} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 
 import {getPrizeInfo} from '../NetworkRequests';
@@ -9,37 +9,51 @@ export default class PrizeInfo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            prizes : []
+            prizes: [],
+            loading: false,
         }
     }
 
-    componentDidMount() {
-        this.props.prizes.map((prize)=>getPrizeInfo(prize)
-            .then((prize)=>this.setState(prevState=>({
-                prizes : [...prevState.prizes,prize]
-            }))))
+    componentWillMount() {
+        //for each prize won get prize info
+        this.props.prizes.map((prize, index) => {
+            //set loading true because it's going to do a network request
+            this.setState({loading: true})
+            getPrizeInfo(prize).then((prize) => this.setState(prevState => {
+                let state = {}
+                //if this prize is the last requested set loading false
+                if (index === this.props.prizes.length - 1) state.loading = false
+                //append prize to array of prizes
+                state.prizes = [...prevState.prizes, prize]
+                return state
+            }))
+        })
     }
 
     render() {
-        const {prizes} = this.state;
-        const {name} = this.props;
-        if (prizes.length>0) {
-            let prizeViews = prizes.map((prize) => {
-                let coworkers = prize.laureates.map(({name}) => name)
-                    .filter((laureate) => (laureate !== name))
-                    .map((laureate) => (<Link to={"/pages/" + laureate}>{laureate}</Link>))
-                    .reduce((prev, curr, index) => [prev, ', ', curr]);
-                coworkers.push(" ");
-                return (
-                    <Segment basic key={prize.year}>
-                        <Header
-                            size="small">{(coworkers.length > 0) ? "with " : ""}{coworkers}for {prize.motivation}</Header>
-                        <Header size="medium">Articles about that:</Header>
-                        <WorksComponent works={prize.works}/>
-                    </Segment>
-                )
-            });
-            return (<Container text>{prizeViews}</Container>)
-        } else return null;
+        const {prizes, loading} = this.state
+        const {name} = this.props
+
+        //for each prize
+        let prizeViews = prizes.map((prize) => {
+            //create list of coworkers
+            let coworkers = prize.laureates.map(({name}) => name)
+                .filter((laureate) => (laureate !== name))
+                .map((laureate) => (<Link key={laureate} to={"/pages/" + laureate}>{laureate}</Link>))
+            coworkers = (coworkers.length > 0) ? coworkers.reduce((prev, curr, index) => [prev, ', ', curr]) : null
+            return (
+                <Segment basic key={prize.year}>
+                    <Header
+                        size="small">{(coworkers) ? "with " : ""}{coworkers} for {prize.motivation}</Header>
+                    <Header size="medium">Articles about that:</Header>
+                    <WorksComponent works={prize.works}/>
+                </Segment>
+            )
+        })
+
+        return (
+            <Container text>{prizeViews}<Loader active={loading}/></Container>
+        )
     }
+
 }

7ff0d54d8cef3f592a2fa908c386e1f0af2263f6
federicoB <federicobertani96@gmail.com>
2018-01-27 20:55:37 +0100

    Improved documentations of functions on NetworkRequest.js

--- Frontend/src/NetworkRequests.js
+++ Frontend/src/NetworkRequests.js
@@ -1,28 +1,52 @@
 import request from 'superagent';
 
+/**
+ * Log in into authentication backend
+ * @param email: string
+ * @param password: string
+ */
 export function logIn(email, password) {
     return request
         .post('/rest-auth/login/')
         .send({email: email, password: password})
 }
 
+/**
+ * Sign up a new user
+ * @param username: string
+ * @param email: string
+ * @param password1: string
+ * @param password2: string
+ */
 export function signUp(username, email, password1, password2) {
     return request
         .post('/rest-auth/registration/')
         .send({username: username, email: email, password1: password1, password2: password2})
 }
 
+/**
+ * Perform log to authentication backend
+ * @returns {*}
+ */
 export function logOut() {
     return request
         .post('/rest-auth/logout/')
 }
 
+/**
+ * Get logged in user info like username
+ * @return {Promise}
+ */
 export function getLoggedUserInfo() {
     return request
         .get("/rest-auth/user/")
         .set('Authorization', "Token " + sessionStorage.getItem('token'))
 }
 
+/**
+ * Returns all laureates with name, pictures and prizes won
+ * @returns {Array}
+ */
 export function getLaureates() {
     return request
         .get('/api/laureates/')
@@ -30,6 +54,11 @@ export function getLaureates() {
         .then((response) => response.body)
 }
 
+/**
+ * Get laureate detailed info like biography and works
+ * @param name: string
+ * @returns {Object}
+ */
 export function getLaureateInfo(name) {
     return request
         .get('/api/laureates/' + name + "/")
@@ -37,6 +66,11 @@ export function getLaureateInfo(name) {
         .then((response) => response.body)
 }
 
+/**
+ * Given the year of the prize return the laureates of that year
+ * @param prizeURl the URI that identify a prize, using its year
+ * @returns Promise
+ */
 export function getPrizeInfo(prizeURl) {
     return request
         .get(prizeURl)
